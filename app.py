b378c95e261a523c3f1a07b66fcb9adf6a7c02b0
Pedro Cavalcante <pedrovitorl@gmail.com>
2022-01-21 23:15:14 -0300

    atualizações

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 from iq import profits
 from threading import Thread
 
 app = Flask(__name__)
+
+# app.config.from_object(__name__)
+sess = Session()
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -10,6 +14,9 @@ def home():
     if request.method == "POST":
 
         valores = request.form["sinal"]
+        session['email'] = request.form["email"]
+        session['senha'] = request.form["senha"]
+        session['valor'] = int(request.form["valor"])
         with open('sinais.txt', 'w') as lista:
             for sinal in valores.split(' '):
                 lista.write(f'{sinal}\n')
@@ -25,11 +32,11 @@ def home():
 
 @app.route('/sinal', methods=["POST", "GET"])
 def sinal():
-    if request.method == 'POST':
+    if request.method == 'GET':
 
-        email = request.form["email"]
-        senha = request.form["senha"]
-        valor = int(request.form["valor"])
+        email = session.get('email')
+        senha = session.get('senha')
+        valor = session.get('valor')
         print("A Executar")
         p1 = Thread(target=profits, args=[email, senha, valor])
         p1.start()
@@ -38,4 +45,7 @@ def sinal():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.secret_key = 'supersecret'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess.init_app(app)
+    app.run(debug=True)
