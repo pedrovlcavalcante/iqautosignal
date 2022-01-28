30b2fb49374cdfd4744cb1bb9934bdd80aea6946
Pedro Cavalcante <pedrovitorl@gmail.com>
2022-01-28 00:43:15 -0300

    meta

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 from iq import profits
-from threading import Thread
+from threading import Thread, active_count, enumerate
 
 app = Flask(__name__)
 app.secret_key = 'supersecret'
@@ -11,13 +11,27 @@ sess = Session()
 
 @app.route('/', methods=["POST", "GET"])
 def home():
+    num_threads = active_count()
+    for t in enumerate():
+        print(t.name)
+    print(num_threads)
     if request.method == "POST":
 
-        valores = request.form["sinal"]
-        session['email'] = request.form["email"]
-        session['senha'] = request.form["senha"]
-        session['valor'] = int(request.form["valor"])
-        session['balance'] = request.form["balance"]
+        try:
+            valores = request.form["sinal"]
+            session['email'] = request.form["email"]
+            session['senha'] = request.form["senha"]
+            session['valor'] = int(request.form["valor"])
+            session['balance'] = request.form["balance"]
+            session['meta'] = int(request.form["meta"])
+            session['loss'] = int(request.form["loss"])
+        except Exception as e:
+            print("ERRO NOS DADOS: ", e)
+        try:
+            session['auto'] = request.form["auto"]
+        except:
+            session['auto'] = False
+
         with open('sinais.txt', 'w') as lista:
             for sinal in valores.split(' '):
                 lista.write(f'{sinal}\n')
@@ -28,7 +42,8 @@ def home():
         # p1 = Thread(target=profits, args=[email, senha, valor])
         # p1.start()
         # print("Executado")
-    return render_template('home.html')
+
+    return render_template('home.html', num_threads=num_threads)
 
 
 @app.route('/sinal', methods=["POST", "GET"])
@@ -37,13 +52,25 @@ def sinal():
 
         email = session.get('email')
         senha = session.get('senha')
-        valor = session.get('valor')
+        auto = session.get('auto')
+        if auto:
+            valor = 0
+        else:
+            valor = session.get('valor')
         balance = session.get('balance')
+        meta = session.get('meta')
+        loss = session.get('loss')
         print("A Executar")
-        p1 = Thread(target=profits, args=[email, senha, valor, balance])
+        p1 = Thread(target=profits, args=[
+                    email, senha, valor, balance, auto, meta, loss])
+        p1.setName(f"Sinal {active_count()}")
         p1.start()
+
+        threads = enumerate()
+        num_threads = active_count()
+
         print("Executado")
-    return 'Oi'
+    return render_template('home.html', threads=threads, num_threads=num_threads)
 
 
 if __name__ == '__main__':
--- iq.py
+++ iq.py
@@ -7,7 +7,7 @@ from os import system
 # from iqoptionapi.iqoptionapi.stable_api import IQ_Option
 
 
-def profits(email, senha, valor, balance):
+def profits(email, senha, valor, balance, auto, meta, loss):
 
     sinais = lista_sinais()
 
@@ -19,8 +19,23 @@ def profits(email, senha, valor, balance):
     conecta(api, balance)
     api.update_ACTIVES_OPCODE()
     inicio = api.get_balance()
+    print("auto", auto)
     for sinal in sinais:
+        if auto:
+            valor = 0.01*api.get_balance()
+            if valor < 2:
+                valor = 2
+        print(f'Valor do investimento: {valor}')
+        print(f'Meta: {meta}% | R$ {0.01*meta*inicio}')
+        print(f'Stop: {-loss}% | R$ {0.01*loss*inicio}')
+        # Verificação de meta diária
 
+        if ((api.get_balance() - inicio) / inicio)*100 >= meta:
+            print("Objetivo diário atingido")
+            break
+        elif ((api.get_balance() - inicio) / inicio)*100 <= -loss:
+            print("Stop Loss diário atingido")
+            break
         print(
             f'Aguardando Sinal: {sinal.par}, {sinal.hora}, {sinal.direcao}, {sinal.expiracao}')
         horario = Horario(sinal)
@@ -84,11 +99,5 @@ def profits(email, senha, valor, balance):
                     else:
                         print("Não foi possível comprar")
                         break
-        if ((api.get_balance() - inicio) / inicio) >= 0.05:
-            print("Objetivo diário atingido")
-            break
-        elif ((api.get_balance() - inicio) / inicio) <= 0.05:
-            print("Stop Loss diário atingido")
-            break
 
     print('Fim da Lista')
